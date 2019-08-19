540677440d54d3b3fdb968f9d65f9fad26e12a08
sunny-zz <b695340402@163.com>
2019-08-19 15:45:19 +0800

    react-bushu

---
+++ precache-manifest.9bad5c97e96f2b2e93cda3ec4c787127.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "48a3a4c6efd0caff480e26e9979b7c34",
+    "url": "/react-bushu/index.html"
+  },
+  {
+    "revision": "2b8777fe75662b64d7f7",
+    "url": "/react-bushu/static/css/main.685dea8f.chunk.css"
+  },
+  {
+    "revision": "512dfd0348bc6108ce64",
+    "url": "/react-bushu/static/js/2.7355a659.chunk.js"
+  },
+  {
+    "revision": "2b8777fe75662b64d7f7",
+    "url": "/react-bushu/static/js/main.31fe82ba.chunk.js"
+  },
+  {
+    "revision": "b1ea01805cda07faae84",
+    "url": "/react-bushu/static/js/runtime~main.d25e4b4b.js"
+  }
+]);
